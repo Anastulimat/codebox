6a35e8663c5c2409eb0bf0cb37fc73c4df504952
Anas TULIMAT <anas.tulimat@orange.com>
2025-12-19 09:49:05 +0100

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import {Button} from "@/components/ui/button";
+
+// ----------------------------------------------------------------------
+
+export default function Home() {
+    return (
+        <div>
+            <h1 className="font-game">Welcome to home page</h1>
+            <Button>
+                subscribe
+            </Button>
+        </div>
+    );
+}
