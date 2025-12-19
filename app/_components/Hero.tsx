1f69ffddf35035c6529fc5d493d07fa2952de3b3
Anas TULIMAT <anas.tulimat@orange.com>
2025-12-19 15:16:42 +0100

    feat: add hero and lading screen

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -1,9 +1,39 @@
 import React from 'react';
+import Image from "next/image";
+import {Button} from "@/components/ui/button";
 
 const Hero = () => {
     return (
-        <div>
-            Hero
+        <div className="w-full relative h-screen overflow-hidden">
+            <Image
+                src={'/hero.gif'}
+                alt={'Hero'}
+                width={1000}
+                height={1000}
+                className="w-full hfull object-cover absolute inset-0"
+            />
+
+            <div className="absolute w-full flex flex-col items-center mt-24">
+                <h2 className="font-bold text-7xl font-game">
+                    Start your
+                </h2>
+                <h2
+                    className="font-bold text-8xl font-game text-yellow-400"
+                    style={{
+                        textShadow: '2px 2px 0 #000, -2px -2px 0 #000, 2px -2px 0 #000, -2px 2px 0 #000'
+                    }}
+                >
+                    Coding adventure
+                </h2>
+
+                <h2 className="mt-5 font-game text-3xl">
+                    Beginner friendly coding courses and projects
+                </h2>
+
+                <Button className=" font-game text-3xl p-6 mt-7" variant="pixel">
+                    Get Started
+                </Button>
+            </div>
         </div>
     );
 };
